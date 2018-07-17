61361429afebe6f8b4e511c545f3e625ac900809
Liang Shi <shiliang318@163.com>
2018-07-17 23:36:58 +0800

    add outline effect

---
+++ Assets/Resources/OutLine/Outline and ScreenSpace texture.shader
@@ -0,0 +1,77 @@
+﻿Shader "Unlit/Outline and ScreenSpace texture"
+{
+	Properties
+	{
+		[Header(Outline)]
+		_OutlineVal("Outline value",Range(0.,2.)) = 1.
+		_OutlineCol("Outline color",Color) = (1.,1.,1.,1.)
+		[Header(Texture)]
+		_MainTex ("Texture", 2D) = "white" {}
+		_Zoom("Zoom",Range(0.5,20)) = 1
+		_SpeedX("Speed along X",Range(-1,1)) = 0
+		_SpeedY("Speed along Y",Range(-1,1)) = 0
+	}
+	SubShader
+	{
+		Tags {"Queue" = "Geometry" "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			Cull Front
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+			struct v2f
+			{
+				float4 pos : SV_POSITION;
+			};
+			float _OutlineVal;
+			
+			v2f vert (appdata_base v)
+			{
+				v2f o;
+				//v.vertex.xyz += v.normal * _OutlineVal;
+				//Convert to vertex to clip space
+				o.pos = UnityObjectToClipPos(v.vertex);
+				//Convert normal to view space(camera space)
+				float3 normal = mul((float3x3)UNITY_MATRIX_IT_MV,v.normal);
+				//Compute normal value in clip space
+				normal.x *= UNITY_MATRIX_P[0][0];
+				normal.y *= UNITY_MATRIX_P[1][1];
+				//Scale the model depending the previous computed normal and outline value
+				//o.pos.xy += _OutlineVal * normal.xy;
+				o.pos.x += _OutlineVal * normal.x;
+				o.pos.y += _OutlineVal * normal.y;
+				return o;
+			}
+			fixed4 _OutlineCol;
+			fixed4 frag (v2f i) : SV_Target
+			{
+				return _OutlineCol;
+			}
+			ENDCG
+		}
+		Pass{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+			float4 vert(appdata_base v):SV_POSITION{
+				return UnityObjectToClipPos(v.vertex);
+			}
+			sampler2D _MainTex;
+			float _Zoom;
+			float _SpeedX;
+			float _SpeedY;
+			fixed4 frag(float4 i:VPOS):SV_TARGET{
+				return tex2D(_MainTex,((i.xy/_ScreenParams.xy)+float2(_Time.y*_SpeedX,_Time.y*_SpeedY))/_Zoom);
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Scripts/DrawNormal.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class DrawNormal : MonoBehaviour {
+	public SkinnedMeshRenderer render;
+	[SerializeField]
+	private bool _displayWireframe = false;
+	[SerializeField]
+	private NormalsDrawData _faceNormals = new NormalsDrawData(new Color32(34,221,221,155),true);
+	[SerializeField]
+	private NormalsDrawData _vertexNormals = new NormalsDrawData(new Color32(200,255,195,127),true);
+
+	void OnDrawGizmosSelected(){
+		EditorUtility.SetSelectedRenderState(render,_displayWireframe?EditorSelectedRenderState.Wireframe:EditorSelectedRenderState.Hidden);
+		OnDrawNormals(true);
+	}
+	public void OnDrawGizmos() {
+		if(!Selection.Contains(this)){
+			OnDrawNormals(true);
+		}
+	}
+
+	private void OnDrawNormals(bool isSelected){
+		var mesh = render.sharedMesh;
+		if(_faceNormals.CanDraw(isSelected)){
+			var triangles = mesh.triangles;
+			var vertices = mesh.vertices;
+			for(var i = 0;i<triangles.Length;i+=3){
+				var v0 = transform.TransformPoint(vertices[triangles[i]]);
+				var v1 = transform.TransformPoint(vertices[triangles[i+1]]);
+				var v2 = transform.TransformPoint(vertices[triangles[i+2]]);
+				var center = (v0+v1+v2)/3;
+				var dir = Vector3.Cross(v1-v0,v2-v0);
+				dir /= dir.magnitude;
+				_faceNormals.Draw(center,dir);
+			}
+		}
+		if(_vertexNormals.CanDraw(isSelected)){
+			var vertices = mesh.vertices;
+			var normals = mesh.normals;
+			for(var i = 0;i<vertices.Length;i++){
+				_vertexNormals.Draw(transform.TransformPoint(vertices[i]),transform.TransformVector(normals[i]));
+			}
+		}
+	}
+
+	[System.Serializable]
+	private class NormalsDrawData{
+		[SerializeField]
+		protected DrawType _draw = DrawType.Selected;
+		protected enum DrawType{Never,Selected,Always}
+		[SerializeField]
+		protected float _length = 0.3f;
+		[SerializeField]
+		protected Color _normalColor;
+		private Color _baseColor = new Color32(255,133,0,255);
+		private const float _baseSize = 0.0125f;
+		public NormalsDrawData(Color normalColor,bool draw){
+			this._normalColor = normalColor;
+			this._draw = draw ? DrawType.Selected:DrawType.Never;
+		}
+		public bool CanDraw(bool isSelected){
+			return (this._draw == DrawType.Always)||(this._draw == DrawType.Selected && isSelected);
+		}
+
+		public void Draw(Vector3 from,Vector3 direction){
+			if(Camera.current.transform.InverseTransformDirection(direction).z<0f){
+				Gizmos.color = _baseColor;
+				Gizmos.DrawWireSphere(from,_baseSize);
+				Gizmos.color = _normalColor;
+				Gizmos.DrawRay(from,direction*_length);
+			}
+		}
+	}
+}
